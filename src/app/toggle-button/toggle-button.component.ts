58646cbb641020dc0ba58385f372d91924f15409
Troy <398798254@qq.com>
2020-09-17 20:25:55 +0800

    a

--- src/app/toggle-button/toggle-button.component.ts
+++ src/app/toggle-button/toggle-button.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Output, EventEmitter, Input, ViewChild } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild, OnChanges } from "@angular/core";
 
 @Component({
   selector: "app-toggle-button",
   templateUrl: "./toggle-button.component.html",
   styleUrls: ["./toggle-button.component.scss"],
 })
-export class ToggleButtonComponent implements OnInit {
+export class ToggleButtonComponent implements OnInit,OnChanges {
+  
+  
   constructor() {}
   @Output() toggleValue = new EventEmitter<any>();
   @Input() switchValue: boolean = false;
@@ -18,17 +20,26 @@ export class ToggleButtonComponent implements OnInit {
   ngOnInit() {}
   color ="white"
   public switchButtonClick(): void {
-    if(!this.switchValue){
-      console.log(this.label.nativeElement)
-      this.color="grey"
-    }else{
-      this.color="white"
-    }
-    console.log(!this.switchValue);
+    // if(!this.switchValue){
+    //   console.log(this.label.nativeElement)
+    //   this.color="grey"
+    // }else{
+    //   this.color="white"
+    // }
+    //console.log(!this.switchValue);
     this.toggleValue.emit({
       title: this.title,
       questionValue: this.questionValue,
       checked: this.switchValue
     });
+  }
+  ngOnChanges(): void {
+    console.log("toggle changed.");
+    if(this.label&&this.switchValue){
+      console.log(this.label.nativeElement)
+      this.color="grey"
+    }else{
+      this.color="white"
+    }
   }
 }
